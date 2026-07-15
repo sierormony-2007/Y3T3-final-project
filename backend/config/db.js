5b18f3d2f81507281b491d962b69cc247417d79f
Sarong Kimsorng <ounkimsorng721@gmail.com>
2026-07-15 13:59:02 +0700

    check

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'EWASTE_db',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASS || '',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
     logging: false,
   }
 );
