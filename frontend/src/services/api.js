32b4ada16cc585fd12e543157d322705705edc43
Sarong Kimsorng <ounkimsorng721@gmail.com>
2026-07-15 23:25:28 +0700

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 // src/services/api.js
-const BASE = '/api';
+const BASE = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api';
+import.meta.env.VITE_API_URL
 
 function getToken() {
   return localStorage.getItem('token');
